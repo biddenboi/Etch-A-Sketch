cf776edb1689e6d381b3bbaee881486aed0d995b
Yujin <yujinpetercho@gmail.com>
2024-01-13 10:44:03 -0600

    added default functionality for the enterance screen to game (startingBoard)

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const board = document.querySelector(".board");
 
+function startingBoard() {
+    const title = document.createElement("h1");
+    const startButton = document.createElement("button");
+
+    title.textContent = "Etch A Sketch.";
+    startButton.textContent = "Begin?";
+
+    
+
+    board.appendChild(title);
+    board.appendChild(startButton);
+}
+
+function gameplayBoard() {
+
+}
+
